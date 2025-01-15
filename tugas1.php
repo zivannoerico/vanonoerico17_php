5ec88ad1eccbad434688e170c998430440de3cf0
zivannoerico <fanoerico@gmail.com>
2025-01-16 06:52:27 +0700

    tes

--- tugas1.php
+++ tugas1.php
@@ -7,7 +7,26 @@
 </head>
 <body>
     <?php
+    define('gaji', 5000000);
+    define('bonus', 500000);
+    define('pajak', 0.12);
+    $proyek = 3;
+    $total_gaji;
+    $gaji_bersih;
     
+
+    $proyek *= bonus;
+    $total_gaji = gaji + $proyek;
+    $total_pajak = $total_gaji * pajak;
+    $gaji_bersih = $total_gaji - $total_pajak;
+
+
+
+
+    echo "ini bonus anda $proyek <br/>";
+    echo "ini gaji anda setelah ditambah bonus $total_gaji <br/>";
+    echo "pajak anda $total_pajak <br/>";
+    echo "ini gaji bersih anda sekarang $gaji_bersih";
     ?>
 </body>
 </html>
